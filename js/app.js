4318e3ece6486836847849054c7aaa752e3ecc87
Sue Duclos <sue.duclos@gmail.com>
2019-11-09 17:14:14 -0800

    added random number function and render function

--- js/app.js
+++ js/app.js
@@ -13,7 +13,29 @@ var seattleShop = {
   averageCookiesPerCustomer: 6.3,
   customersEachHour:[],
   cookiesEachHour:[],
-  totalCookiesForTheDay: 0
+  totalCookiesForTheDay: 0,
 
+  render:  function() {
+    for( var i = 0; i < hours.length; i++ ) {
+      //create element
+      var listElement = document.createElement('li');
+      //give element content
+      listElement.textContent = `${hours[i]} Cookies: 34`;
+      //append to DOM
+      seattleElement.appendChild(listElement);
+    }
+  },
 
+  randomNumber: function(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+  },
+
+  customersEachHour() {
+    for
+
+  }
 };
+
+
+seattleShop.render();
+
