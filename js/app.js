a02a1bd0b12764c040b428429145dfff6a8e6129
Sue Duclos <sue.duclos@gmail.com>
2019-11-13 13:09:26 -0800

    updating functions

--- js/app.js
+++ js/app.js
@@ -1,19 +1,28 @@
 'use strict';
 
-console.log('proof of life');
+//console.log('proof of life');
 
 var seattleElement = document.getElementById('seattleShop');
 
 
-var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm'];
+var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 
 var seattleShop = {
+  storeName: 'Seattle HQ',
   minCustomersEachHour: 23,
   maxCustomersEachHour: 65,
   averageCookiesPerCustomer: 6.3,
-  customersEachHour:[],
-  cookiesEachHour:[],
+  customersEachHourArray:[],
+  cookiesEachHourArray:[],
   totalCookiesForTheDay: 0,
+
+  calculateCustomers: customersEachHour() {
+  
+  }
+
+  randomNumber: function(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
 
   render:  function() {
     for( var i = 0; i < hours.length; i++ ) {
@@ -24,18 +33,7 @@ var seattleShop = {
       //append to DOM
       seattleElement.appendChild(listElement);
     }
-  },
-
-  randomNumber: function(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
-  },
-
-  customersEachHour() {
-    for
-
   }
-};
-
 
 seattleShop.render();
 
