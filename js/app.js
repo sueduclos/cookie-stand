fc785a0344690f06d6dcbc21eeb83c649fb038a5
Sue Duclos <sue.duclos@gmail.com>
2019-11-13 15:09:27 -0800

    updated render function

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
 'use strict';
 
 //console.log('proof of life');
+//number of cookies at each store at each hour plus totals
 
-var seattleElement = document.getElementById('seattleShop');
+var hoursArray = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 
-
-var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
-
+//container to put my lists
+var storeSales = document.getElementById('stores');
 var seattleShop = {
   storeName: 'Seattle HQ',
   minCustomersEachHour: 23,
@@ -17,35 +17,41 @@ var seattleShop = {
   totalCookiesForTheDay: 0,
 
   calculateCustomers: function() {
-    for(var i =0; i < hoursArray.length ; i++) {
+    for (var i = 0; i < hoursArray.length; i++) {
       var randomCustomer = getRandom(this.minCustomersEachHour, this.maxCustomersEachHour);
-      this.customersEachHourArray.push(randomCustomer)
+      this.customersEachHourArray.push(randomCustomer);
     }
-  }
+  },
 
   calculateCookies: function() {
     for (var i = 0; i < this.customersEachHourArray.length; i++) {
       var cookies = Math.round(this.customersEachHourArray[i] * this.averageCookiesPerCustomer);
+      this.totalCookiesForTheDay += cookies;
       this.cookiesEachHourArray.push(cookies);
     }
-  }
+  },
 
-
-
-  function getRandom(min, max) {
-    return Math.floor(Math.random() * (max - min +1)) + min;
-  }
-
-  render:  function() {
-    for( var i = 0; i < hours.length; i++ ) {
-      //create element
-      var listElement = document.createElement('li');
-      //give element content
-      listElement.textContent = `${hours[i]} Cookies: 34`;
-      //append to DOM
-      seattleElement.appendChild(listElement);
+  render: function() {
+    this.calculateCustomers();
+    this.calculateCookies();
+    var ulEl = document.createElement('ul');
+    var h2El = document.createElement('h2');
+    h2El.textContent = this.storeName;
+    storeSales.appendChild(h2El);
+    for (var i = 0; i < hoursArray.length; i++ ) {
+      var liEl = document.createElement('li');
+      liEl.textContent = `${hoursArray[i]} : ${this.cookiesEachHourArray[i]} Cookies`; 
+      ulEl.appendChild(liEl);
     }
-  }
+    liEl = document.createElement('li');
+    liEl.textContent = `Total: ${this.totalCookiesForTheDay} Cookies`;
+    ulEl.appendChild(liEl);
+    storeSales.appendChild(ulEl);
+  },
+};
 
+function getRandom(min, max) {
+  return Math.floor(Math.random() * (max - min +1)) + min;
+}
 seattleShop.render();
 
